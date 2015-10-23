6b64f068c2685c6ac4b3e5b05901acac6935a9d8
mlaykj99 <joshgreenwell779@yahoo.com>
2015-10-23 17:20:51 -0500

    Basic Server and Pages
    Implementing grunt and basic server for page routing.

---
+++ Website/Server.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Josh Greenwell on 10/23/2015.
+ */
+//App dependencies and setup
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+/* --- Router functions start --- */
+router.use(function (req,res,next) {
+    console.log("/" + req.method);
+    next();
+});
+
+router.get("/",function(req,res){
+    res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+    res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+    res.sendFile(path + "contact.html");
+});
+/* --- Router functions end --- */
+
+//Add router to the app
+app.use("/",router);
+//Set up 404 page
+app.use("*",function(req,res){
+    res.sendFile(path + "404.html");
+});
+
+//Port the server will listen on
+app.listen(3000,function(){
+    console.log("Live at Port 3000");
+});
