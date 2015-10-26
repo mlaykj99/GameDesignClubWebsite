8f451a29f6c6470032bd6940220d5245b33c25de
mlaykj99 <joshgreenwell779@yahoo.com>
2015-10-26 12:54:41 -0500

    Header In Play

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,3 +1,30 @@
 /**
  * Created by Josh on 10/23/2015.
  */
+
+$( document ).ready ( function ()
+{
+    updateHeader( 'home', 'li1', 'h' );
+});
+
+function loadFragment ( page )
+{
+    $( '#container').load( './views/' + page + '.html' );
+}
+
+function updateHeader ( page, liId, aId )
+{
+    setHeaderActive( liId, aId );
+    loadFragment( page );
+}
+
+function setHeaderActive( liId, aId )
+{
+    var a = $( '#' + aId);
+
+    $(document).find('.active').removeClass('active');
+    $(document).find('.sr-only').remove();
+
+    $( '#' + liId ).addClass( 'active' );
+    a.html( a.html() + '<span class="sr-only">(current)</span>' );
+}
